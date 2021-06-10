6bc6550ad034733f837cd5eaf12938e61f59908e
Nicolás Sánchez <nicksansa12@gmail.com>
2021-06-10 10:53:18 -0500

    Nuevas clases xd :D

---
+++ PlantillaJuego/src/Model/Cat.java
@@ -0,0 +1,264 @@
+package Model;
+
+import processing.core.PApplet;
+import processing.core.PImage;
+import processing.core.PVector;
+
+
+public class Cat extends Thread implements drawable{
+
+	private PApplet app;
+	private PImage revy;
+
+	private int floor;
+	private int height;
+	private int width;
+	private int posX;
+	private int posXCollision;
+	private PVector position;
+	private PVector vel;
+	private PVector cc;
+	private boolean jump;
+	private int key;
+	private int speed;
+	private int posY;
+	private boolean move;
+	private boolean fall;
+
+	public Cat(int x, int y,PApplet app) {
+
+		posX = x;
+		revy= app.loadImage("../Resources/revy1.png");
+		position= new PVector(x,y);
+		vel= new PVector(0,0);
+		cc= new PVector(0,0);
+		floor= 500;
+		height=40;
+		jump=false;
+		this.app = app;
+		speed=2;
+		move= false;
+		fall = false;
+	}
+
+	@Override
+	public void run() {
+		move();
+		try {
+			sleep(100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void move() {
+
+		notMove();
+
+		if (move == true) {
+
+			if(key==37) {
+				vel.x=-5;
+				cc.x=0.5f;
+			}
+
+			if(key==39) {
+				vel.x=8;
+				cc.x=-0.7f;
+			}
+
+			if(key==38 && jump==false) {
+				jump=true;
+				vel.y=-40;
+			}
+		}
+	}
+
+	public void notMove() {
+
+		if (key==37 ||key==39 || key==38 ) {
+			move=true;
+		}else {
+			move = false; 
+		}
+	}
+
+	public int getPosXCollision() {
+		return posXCollision;
+	}
+
+	public void setPosXCollision(int posXCollision) {
+		this.position.x = posXCollision;
+	}
+
+
+	public int getPosX() {
+		return posX;
+	}
+
+	public void setPosX(int posX) {
+		this.posX = posX;
+	}
+
+	public PVector getVel() {
+		return vel;
+	}
+
+	public void setVel(PVector vel) {
+		this.vel = vel;
+	}
+
+	public PVector getCc() {
+		return cc;
+	}
+
+	public void setCc(PVector cc) {
+		this.cc = cc;
+	}
+
+	public boolean isJump() {
+		return jump;
+	}
+
+	public void setJump(boolean jump) {
+		this.jump = jump;
+	}
+
+	public boolean isMove() {
+		return move;
+	}
+
+	public void setMove(boolean move) {
+		this.move = move;
+	}
+
+	public PApplet getApp() {
+		return app;
+	}
+
+	public void setApp(PApplet app) {
+		this.app = app;
+	}
+
+	public PImage getRevy() {
+		return revy;
+	}
+
+	public void setRevy(PImage revy) {
+		this.revy = revy;
+	}
+
+	public int getFloor() {
+		return floor;
+	}
+
+	public void setFloor(int floor) {
+		this.floor = floor;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public PVector getPosition() {
+		return position;
+	}
+
+	public void setPosition(PVector position) {
+		this.position = position;
+	}
+
+	public int getKey() {
+		return key;
+	}
+
+	public void setKey(int c) {
+		this.key = c;
+	}
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+
+	public int getPosY() {
+		return posY;
+	}
+
+	public void setPosY(int posY) {
+		this.posY = posY;
+	}
+
+	public boolean isFall() {
+		return fall;
+	}
+
+	public void setFall(boolean fall) {
+		this.fall = fall;
+	}
+
+	@Override
+	public void drawChar() {
+		cc.y= 3.5f;
+		if(position.x<=(-1+position.x)|| position.x>=1190) {
+			cc.x *=-1;
+			vel.x*=-1;
+		}
+
+		app.image(revy, position.x, position.y);
+		position.add(vel);
+		vel.add(cc);
+
+
+		if(vel.x>-1 && vel.x<1) {
+			cc.x=0;
+			vel.x=0;
+		}
+		
+		if (fall == false) {
+			if(position.y>=floor-height-5) {
+				jump=false;
+				position.y=floor-height;
+				vel.y=0;
+				cc.y=0;
+			}
+		} else if (fall == true ){
+			vel.y = 40;
+		}
+		
+		posY = (int) position.y;
+		posXCollision = (int) position.x;
+	}
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
--- PlantillaJuego/src/Model/Logic.java
+++ PlantillaJuego/src/Model/Logic.java
@@ -1,5 +1,362 @@
 package Model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.LinkedList;
+
+import exception.Lose;
+import processing.core.PApplet;
+
 public class Logic {
+
+	private PApplet app;
+	private boolean move;
+	private ByName byname;
+	private ByTime bytime;
+	private ByDate bydate;
+
+	private int min,seg;
+	private boolean time;
+	private boolean loseTouch;
+
+	private String temporalName;
+	private int posXEnemy;
+	private LinkedList<Player> player;
+	private ArrayList<Enemy> enemy;
+
+	private ArrayList <Star> star;
+
+	private Revy revy;
+
+	private int score;
+
+
+	private static Logic oneInstance;
+
+	private Logic(PApplet app) {
+
+		this.app=app;
+		revy = new Revy(50, 457, app);
+		byname= new ByName();
+		bytime= new ByTime();
+		bydate= new ByDate();
+		temporalName="";
+		score=0;
+		loseTouch=false;
+
+		min=0;
+		seg=0;
+		time=false;
+
+		player = new LinkedList<Player>();
+		enemy = new ArrayList<Enemy>();
+		star = new ArrayList <Star> ();
+
+		try {
+			createEnemy();
+			createStar ();
+		} catch (RuntimeException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+
+
+
+	} //CONSTRUCTOR
+
+
+
+	public static Logic getInstance(PApplet app) {
+		if(oneInstance == null) {
+			oneInstance = new Logic(app);
+		}
+		return oneInstance;
+	}
+
+	public void registerPlayer(String name) {
+		temporalName=name;
+	}
+
+	public void drawGame() {
+		revy.drawChar();
+		if (move == true) {
+			Thread revyMove = new Thread(revy);
+			revyMove.start();
+		}
+
+		for (int i = 0; i < star.size(); i++) {
+			star.get(i).setMoveXStar(enemy.get(0).isMoveXEnemy());
+			Thread starMove = new Thread(star.get(i));
+			starMove.start();
+		}
+
+		time=true;
+		paintTime();
+		newPoint();
+		collision();
+	}
+
+	public void newPoint() {
+		for (int i = 0; i < star.size(); i++) {
+			if (app.dist(revy.getPosXCollision()+66, revy.getPosY()+70, star.get(i).getposXStar(), star.get(i).getposYStar())<=100) {
+				star.remove(i);
+				score = score + 100;
+			}
+		}
+		app.fill(255);
+		app.textSize(40);
+		app.text("Score: "+ score, 700, 750);
+	}
+
+	public void fallRevy(boolean c) {
+		revy.setFall(c);
+	}
+
+	public void drawEnemy() {
+
+		for (int i = 0; i < enemy.size(); i++) {
+			enemy.get(i).drawChar();
+			Thread newEnemy = new Thread(enemy.get(i));
+			newEnemy.start();
+			//System.out.println(enemy.get(0).getMoveX());
+		}
+
+		moveEnemy();
+
+	}
+
+	public void moveEnemy() {
+
+		for (int i = 0; i < enemy.size(); i++) {
+
+			enemy.get(i).moveEnemy();
+		}
+	}
+
+
+	public void createEnemy () {
+
+		enemy.add(new Enemy(app, 946, 183, 1));
+		enemy.add(new Enemy(app, 1206, 450, -1));
+		enemy.add(new Enemy(app, 1463, 184, -1));
+		enemy.add(new Enemy(app, 2037, 148, 1));
+		enemy.add(new Enemy(app, 2461, 451, -1));
+
+		System.out.println(enemy.size());
+	}
+
+	public void createStar () {
+
+		star.add(new Star (app, 414,490));
+
+		star.add(new Star (app, 683,382));
+
+		star.add(new Star (app, 975,238));
+
+		star.add(new Star (app, 1515,238));
+
+		star.add(new Star (app, 2068,211));
+
+		star.add(new Star (app, 2094,493));
+
+		star.add(new Star (app, 2343,490));
+
+		star.add(new Star (app, 2833,326));
+
+		star.add(new Star (app, 3083,326));
+
+	}
+
+	public void drawStars () {
+
+		for (Star star : star) {
+			star.draw();
+		}
+
+	}
+
+	public void paintTime() {
+
+		if (time==true) {
+
+
+			if (app.frameCount % 60 == 0) {
+				seg += 1;
+			}
+			if (seg == 60) {
+				seg = 0;
+				min += 1;
+			}
+		}
+		app.fill(255);
+		app.textSize(40);
+		app.text("Time: "+ min + ":" + seg, 400, 750);
+	}
+
+	public void loseGame(){
+
+		time=false;
+		String m= Integer.toString(min);
+		String s= Integer.toString(seg);
+		String time = m+":"+s;
+		Date date = new Date();
+		for (int i = 0; i < 1; i++) {
+
+			Player newPlayer = new Player(temporalName, date, time, score, app);
+			player.add(newPlayer);
+
+			System.out.println("player" + player.size());
+		}
+	}
+
+	public void reset() {
+
+		revy = new Revy(50, 457, app);
+		enemy.clear();
+		star.clear();
+		temporalName="";
+		score=0;
+		loseTouch=false;
+
+		min=0;
+		seg=0;
+
+		createEnemy();
+		createStar ();
+	}
+
+
+	public void collision() {
+		for (int i = 0; i < enemy.size(); i++) {
+			if (app.dist(revy.getPosXCollision()+ 80, revy.getPosY()+ 10, enemy.get(i).getPosXEnemy()+ 80 , enemy.get(i).getPosYEnemy()+ 20) <= 20) {
+				loseTouch = true;
+				loseGame();
+				revy.setPosXCollision(0);
+			}
+			if (app.dist(revy.getPosXCollision()+ 66, revy.getPosY()+ 140, enemy.get(i).getPosXEnemy()+ 50 , enemy.get(i).getPosYEnemy()) <= 40) {
+				enemy.remove(i);
+			}
+
+		}		
+	}
+
+
+	public void drawData() {
+
+		for (int i = 0; i < player.size(); i++) {
+			player.get(i).drawData(180, 370+(50*i));
+		}
+	}
+
+	public void getKey(int c) {
+		revy.setKey(c);
+
+		if (c == 37 || c== 38 || c == 39) {
+			move=true;
+		}
+	}
+
+	public void notMove (int c) {
+		int not = c;
+		if (not == 37 || not== 38 || not == 39) {
+			move=false;
+		}
+	}
+
+
+	public int getScore() {
+		return score;
+	}
+
+
+
+	public void setScore(int score) {
+		this.score = score;
+
+	}
+
+	public int getPosX() {
+		return revy.getPosX();
+	}
+
+
+
+
+	public LinkedList<Player> getPlayer() {
+		return player;
+	}
+
+	public void setPlayer(LinkedList<Player> player) {
+		this.player = player;
+	}
+
+
+	public int getPosY() {
+		return revy.getPosY();
+	}
+
+
+
+	public boolean isLoseTouch() {
+		return loseTouch;
+	}
+
+
+
+	public void setLoseTouch(boolean loseTouch) {
+		this.loseTouch = loseTouch;
+	}
+
+
+	public void organizeByName()
+	{
+		Collections.sort(player, byname);
+	}
+
+	public void organizeByScore()
+	{
+		Collections.sort(player);
+	}
+
+	public void organizeByDate()
+	{
+		Collections.sort(player, bydate);
+	}
+
+	public void organizeByTime()
+	{
+		Collections.sort(player, bytime);	
+	}
+	public void setPosXRevy(int posXCollision) {
+		revy.setPosXCollision(posXCollision);
+
+	}
+
+	public int getXCollision() {
+		return revy.getPosXCollision();
+	}
+
+	public void setPosX(int c) {
+		for (int i = 0; i < enemy.size(); i++) {
+			enemy.get(i).setMoveX(c);
+		}
+	}
+
+	public int getPosXEnemy(){
+		for (int i = 0; i < enemy.size(); i++) {
+			posXEnemy= enemy.get(i).getMoveX();	
+		}
+		return posXEnemy;
+	}
+
+	public void setIsMoving(boolean c){
+		for (int i = 0; i < enemy.size(); i++) {
+			enemy.get(i).setMoveXEnemy(c);
+		}
+	}
+
+
+
 
 }
